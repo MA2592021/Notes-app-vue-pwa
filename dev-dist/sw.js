fd50c13b20febcd85acd016673beb507a6d21a33
ma2592021 <amirsaid2085@gmail.com>
2024-02-27 18:46:20 +0200

    feat:install message on ios

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4hos5mfeg8o"
+    "revision": "0.lieplaobopo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
   <router-view />
+  <v-dialog v-model="dialog" width="auto">
+    <v-card prepend-icon="mdi-download" fullscreen title="Install The App">
+      <template v-slot:actions>
+        <v-btn class="ms-auto" text="Ok" @click="dialog = false"></v-btn>
+      </template>
+      <v-card-text class="mt-3">
+        <v-row>
+          <v-col cols="12">
+            <div class="d-flex align-center">
+              <v-icon size="x-large">mdi mdi-export-variant</v-icon>
+              <h4 class="ml-3">Press on the Options button</h4>
+            </div>
+          </v-col>
+          <v-col cols="12">
+            <div class="d-flex align-center">
+              <v-icon size="x-large">mdi mdi-plus-box</v-icon>
+              <h4 class="ml-3">Press on Add to Home Screen</h4>
+            </div>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup>
+import { ref } from "vue";
+const dialog = ref(false);
 const isIos = () => {
   const userAgent = window.navigator.userAgent.toLowerCase();
   return /iphone|ipad|ipod/.test(userAgent);
@@ -13,6 +38,6 @@ const isInStandaloneMode = () =>
 
 // Checks if should display install popup notification:
 if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
+  dialog.value = true;
 }
 </script>
